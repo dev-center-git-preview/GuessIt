06ac846fe1ba208fae16ff775140da59333774d6
dev-center-git-preview <79448806+dev-center-git-preview@users.noreply.github.com>
2021-03-02 21:33:53 +0200

    Created GameViewModel class

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -4,6 +4,9 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+    init {
+        Log.i("GameViewModel","GameViewModel created")
+    }
     override fun onCleared() {
         super.onCleared()
         Log.i(this.javaClass.name, "onCleared")
